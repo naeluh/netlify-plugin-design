ae66dc0e3fd37cbbc2f4fd88206e5ad25af1dd0d
naeluh <naeluh@gmail.com>
2020-02-11 21:42:27 -0800

    update plugin

--- index.js
+++ index.js
@@ -1,18 +1,15 @@
-const NetlifyAPI = require("netlify");
+const NetlifyAPI = require('netlify');
+require('dotenv').config();
 
 module.exports = {
-  name: "netlify-plugin-hello-world",
-  onInit: async ({ utils, constants, pluginConfig, netlifyConfig }) => {
-    // console.log(utils);
-    // console.log(constants);
-    // console.log(pluginConfig.siteId);
-    // console.log(netlifyConfig);
+  name: 'netlify-plugin-design',
+  onInit: async ({ utils, constants, netlifyConfig }) => {
+    console.group(utils, constants, netlifyConfig);
 
-    const siteId = pluginConfig.siteId;
-    const client = new NetlifyAPI(pluginConfig.siteToken);
-    const sitesDeploys = await client.listSiteDeploys({ siteId: siteId });
+    const client = new NetlifyAPI(process.env.SITE_TOKEN);
+    const sitesDeploys = await client.listSiteDeploys({ siteId: process.env.SITE_ID });
 
-    sitesDeploys.map(deploys => {
+    sitesDeploys.map((deploys) => {
       console.log(deploys.commit_url);
     });
 
@@ -20,39 +17,37 @@ module.exports = {
 
     /* Do stuff if files modified */
     if (git.modifiedFiles.length) {
-      console.log("Modified files:", git.modifiedFiles);
+      console.log('Modified files:', git.modifiedFiles);
     }
 
     /* Do stuff only if css code edited */
-    const cssFiles = git.fileMatch("**/*.css");
-    console.log("css files git info:", cssFiles);
+    const cssFiles = git.fileMatch('**/*.css');
+    console.log('css files git info:', cssFiles);
     /* Do stuff only if css files edited */
 
     if (cssFiles.deleted.length !== 0) {
-      console.log(">> Run thing because css files have been deleted\n");
+      console.log('>> Run thing because css files have been deleted\n');
       console.log(cssFiles);
     }
 
     /* Do stuff only if js code edited */
-    const jsFiles = git.fileMatch("**/*.js");
-    console.log("js files git info:", jsFiles);
+    const jsFiles = git.fileMatch('**/*.js');
+    console.log('js files git info:', jsFiles);
 
     /* Do stuff only if html code edited */
-    const htmlFiles = git.fileMatch("**/*.html");
-    console.log("html files git info:", htmlFiles);
+    const htmlFiles = git.fileMatch('**/*.html');
+    console.log('html files git info:', htmlFiles);
 
     if (htmlFiles.edited.length !== 0) {
-      console.log(">> Run thing because HTML has changed\n");
+      console.log('>> Run thing because HTML has changed\n');
     }
     //
     /* Do stuff only if markdown files edited */
-    const markdownFiles = git.fileMatch("**/*.md");
-    console.log("markdown files git info:", markdownFiles);
+    const markdownFiles = git.fileMatch('**/*.md');
+    console.log('markdown files git info:', markdownFiles);
 
     if (markdownFiles.modified.length !== 0) {
-      console.log(
-        ">> Run thing because Markdown files have been created/changed/deleted\n"
-      );
+      console.log('>> Run thing because Markdown files have been created/changed/deleted\n');
     }
   }
   /* onPreBuild: () => {
